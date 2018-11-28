eb60a39bb201699e4a5a361c4754df3f5728fbb9
TopTalentDev <toptal715@gmail.com>
2018-11-28 08:59:40 -0500

    set up for redux and async redux

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+import reducers from './reducers/reducers.js'
+import thunkMiddleware from 'redux-thunk'
+
 import './index.css';
 import App from './container/App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(
+  reducers,
+  applyMiddleware(thunkMiddleware)
+);
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
