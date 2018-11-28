6648e5c04cbade1ebdb33013f59fa51d23e99d9d
TopTalentDev <toptal715@gmail.com>
2018-11-28 09:03:01 -0500

    minor update

--- client/src/presentation/Dropdown.js
+++ client/src/presentation/Dropdown.js
@@ -1,23 +1,27 @@
 import React from 'react';
-import { ButtonToolbar, DropdownButton, MenuItem } from 'react-bootstrap';
+import { ButtonToolbar, Dropdown, MenuItem, Button } from 'react-bootstrap';
 
-function Dropdown(props) {
+function DropdownButtons(props) {
   const handleSelect = (eventKey, event) =>
     props.handleSelect(event)
 
   return (
     <ButtonToolbar>
-      <DropdownButton bsStyle='info' title='Search Category'
-      id='dropdown-basic'>
-        {props.menuItems.map((item, i) =>
-          <MenuItem key={i} eventKey={i} onSelect={handleSelect}
-          value={item}>
-            {item}
-          </MenuItem>
-        )}
-      </DropdownButton>
+      <Dropdown>
+        <Dropdown.Toggle>
+          <Button bsStyle='info' id="button-1"> Select Category </Button>
+        </Dropdown.Toggle>
+        <Dropdown.Menu>
+          {props.menuItems.map((item, i) =>
+            <MenuItem key={i} eventKey={i} onSelect={handleSelect}
+            value={item}>
+              {item}
+            </MenuItem>
+          )}
+        </Dropdown.Menu>
+      </Dropdown>
     </ButtonToolbar>
   )
 }
 
-export default Dropdown;
+export default DropdownButtons;
