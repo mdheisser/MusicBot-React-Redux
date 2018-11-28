4b09949f1ffdf8105e6ef8b66d6b5279f95aa0ce
TopTalentDev <toptal715@gmail.com>
2018-11-28 13:17:43 -0500

    updated dropdown button to toggle button view

--- client/src/presentation/Dropdown.js
+++ client/src/presentation/Dropdown.js
@@ -1,27 +1,42 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import { ButtonToolbar, Dropdown, MenuItem, Button } from 'react-bootstrap';
 
-function DropdownButtons(props) {
-  const handleSelect = (eventKey, event) =>
-    props.handleSelect(event)
+class DropdownButton extends PureComponent {
+  constructor() {
+    super();
+    this.state = {
+      button: 'Select Category'
+    }
+  }
 
-  return (
-    <ButtonToolbar>
-      <Dropdown>
-        <Dropdown.Toggle>
-          <Button bsStyle='info' id="button-1"> Select Category </Button>
-        </Dropdown.Toggle>
-        <Dropdown.Menu>
-          {props.menuItems.map((item, i) =>
-            <MenuItem key={i} eventKey={i} onSelect={handleSelect}
-            value={item}>
-              {item}
-            </MenuItem>
-          )}
-        </Dropdown.Menu>
-      </Dropdown>
-    </ButtonToolbar>
-  )
+  handleSelect = (eventKey, event) =>
+    this.props.handleSelect(event)
+
+  toggleDrop = (eventKey, event) => {
+    this.setState({
+      button: event.target.innerHTML
+    })
+  }
+
+
+  render() {
+    return (
+      <ButtonToolbar>
+        <Dropdown onSelect={this.toggleDrop}>
+          <Button bsStyle='info' id="button-1">{this.state.button}</Button>
+          <Dropdown.Toggle />
+          <Dropdown.Menu>
+            {this.props.menuItems.map((item, i) =>
+              <MenuItem key={i} eventKey={i} onSelect={this.handleSelect}
+              value={item}>
+                {item}
+              </MenuItem>
+            )}
+          </Dropdown.Menu>
+        </Dropdown>
+      </ButtonToolbar>
+    )
+  }
 }
 
-export default DropdownButtons;
+export default DropdownButton;
