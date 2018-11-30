74f6421a577607d54ffb1edc7405f9e823d4f804
TopTalentDev <toptal715@gmail.com>
2018-11-29 22:15:35 -0500

    updated and separated out components for separation of concern

--- client/src/container/App.js
+++ client/src/container/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
-import ReactDOM from 'react'
-import { Jumbotron, Grid, Row, Col } from 'react-bootstrap'
-import SearchForm from './SearchForm'
+import { Panel, Jumbotron, Grid, Row, Col } from 'react-bootstrap'
+import SearchContainer from './SearchContainer'
 import Navbar from '../presentation/Navbar'
+import ProfileContainer from '../presentation/ProfileContainer'
+import WelcomePanel from '../presentation/WelcomePanel'
 import { fetchSearch, getToken } from '../actions/SearchActions'
 
 class App extends Component {
@@ -13,8 +14,8 @@ class App extends Component {
       renderSearch: {
         'form-1': true,
         'form-2': true,
-        'form-3': true
-      }
+      },
+      showProfile: false
     }
   }
   //get auth token after component mounts
@@ -22,24 +23,11 @@ class App extends Component {
     this.props.getToken()
   }
 
-  removeSearch = (keyProp) => {
-    if(keyProp === 'form-1') {
-      this.setState({
-        renderSearch: {
-          ...this.state.renderSearch,
-          'form-1': false
-        }
-      })
-    } else if (keyProp === 'form-2') {
-      this.setState({
-        renderSearch: {
-          ...this.state.renderSearch,
-          'form-2': false
-        }
-      })
-    }
+  showProfile = () => {
+    this.setState({
+      showProfile: true
+    })
   }
-
 
   render() {
     return (
@@ -47,27 +35,21 @@ class App extends Component {
         <Navbar />
         <div className="container-fluid">
           <Grid>
+            <WelcomePanel />
             <Row className="show-grid">
               <Col xs={6} md={6}>
                 <Jumbotron>
-                  <h4>Criteria</h4>
-                  {this.state.renderSearch['form-1'] ?
-                    <SearchForm keyProp='form-1' removeMe={this.removeSearch}/>
-                    : null
-                  }
-
+                  <SearchContainer keyProp='form-1' />
                 </Jumbotron>
               </Col>
               <Col xs={6} md={6}>
                 <Jumbotron>
-                  <h4>Criteria</h4>
-                  {this.state.renderSearch['form-2'] ?
-                    <SearchForm keyProp='form-2' removeMe={this.removeSearch}/>
-                    : null
-                  }
+                  <SearchContainer keyProp='form-2' />
                 </Jumbotron>
               </Col>
             </Row>
+            <ProfileContainer showProfile={this.showProfile}
+            profile={this.state.showProfile}/>
           </Grid>
         </div>
       </div>
