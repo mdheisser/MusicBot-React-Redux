08f7215dbe0799ddfe7755a4726cdd07dd330198
TopTalentDev <toptal715@gmail.com>
2018-12-03 13:18:30 -0500

    added helper method to get access token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::API
+
+  private
+
+  def get_token
+    resp = Faraday.post('https://accounts.spotify.com/api/token') do |req|
+      req.headers = {
+          'Content-Type': 'application/x-www-form-urlencoded',
+          'Authorization': ENV['SPOTIFY_AUTHORIZATION']
+        }
+      req.body = {
+        'grant_type': 'client_credentials'
+      }
+    end
+    @token = 'Bearer ' + JSON.parse(resp.body)['access_token']
+  end
 end
