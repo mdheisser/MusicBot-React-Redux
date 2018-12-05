a27df46c57d7340a8bdcddf9b4db406891ad83b5
TopTalentDev <toptal715@gmail.com>
2018-12-05 15:45:19 -0500

    updated actions to persist like data to server

--- client/src/actions/ProfileActions.js
+++ client/src/actions/ProfileActions.js
@@ -19,6 +19,21 @@ function getRec(rec) {
   }
 }
 
+function saveRecAction(rec) {
+  return {
+    type: 'saveRec',
+    trackID: rec.trackID
+  }
+}
+
+function likeAction(likes) {
+  return {
+    type: 'saveLike',
+    likes: likes
+  }
+}
+
+//create profile and save profile ID to state
 export function createProfile(trackIDs, artistIDs) {
   const fetchData = {
     method: 'POST',
@@ -31,8 +46,22 @@ export function createProfile(trackIDs, artistIDs) {
     )
   }
   return function(dispatch) {
-    fetch(`api/profiles`, fetchData).then(resp => resp.json())
+    fetch(`/api/profiles`, fetchData).then(resp => resp.json())
     .then(json => dispatch(profileAction(json.id))).catch(
       error => dispatch(receiveError(error)))
+  }
+}
+
+//persist the likes data (type and spotify id) to state
+export function saveLike(trackSpotifyID, profileID) {
+  const fetchData = {
+    method: 'POST',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify({spotifyID: trackSpotifyID})
+  }
+  return function(dispatch) {
+    fetch(`/api/profiles/${profileID}/likes`, fetchData)
+    .then(resp => resp.json())
+    .then(json => dispatch(likeAction(json)))
   }
 }
