710409fe4234cc91a9f4a6c0920cc909944a9896
TopTalentDev <toptal715@gmail.com>
2018-12-05 15:45:56 -0500

    minor fixes

--- client/src/container/App.js
+++ client/src/container/App.js
@@ -1,37 +1,31 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
-import { Panel, Jumbotron, Grid, Row, Col } from 'react-bootstrap'
+import { Button, Grid, Row, Col } from 'react-bootstrap'
 import SearchContainer from './SearchContainer'
 import Navbar from '../presentation/Navbar'
 import ProfileContainer from '../presentation/ProfileContainer'
 import WelcomePanel from '../presentation/WelcomePanel'
-import { fetchSearch } from '../actions/SearchActions'
 import { createProfile } from '../actions/ProfileActions'
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      renderSearch: {
-        'form-1': true,
-        'form-2': true,
-      }
+      renderSearch: true
     }
   }
 
   getProfile = () => {
-    const results = Object.values(this.props.searchResults)
+    const result = this.props.searchResults
     var trackIDs = []
     var artistIDs = []
-    for(const result of results) {
-      var keyword = Object.keys(result)[0]
-      if(keyword === 'tracks') {
-        var track = result['tracks']['items'][0]
-        trackIDs.push(track.id)
-      } else if (keyword === 'artists') {
-        var artist = result['artists']['items'][0]
-        artistIDs.push(artist.id)
-      }
+    var keyword = Object.keys(result)[0]
+    if(keyword === 'tracks') {
+      var track = result['tracks']['items'][0]
+      trackIDs.push(track.id)
+    } else if (keyword === 'artists') {
+      var artist = result['artists']['items'][0]
+      artistIDs.push(artist.id)
     }
     //create new profile in db and get profile ID
     this.props.createProfile(trackIDs, artistIDs);
@@ -39,22 +33,22 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <Navbar />
-        <div className="container-fluid">
-          <Grid>
-            <WelcomePanel />
-            <Row className="show-grid">
-              <Jumbotron>
-                <h1>Your song or artist</h1>
-                <SearchContainer keyProp='form-1' />
-              </Jumbotron>
-            </Row>
-            <ProfileContainer getProfile={this.getProfile}
-            profile={this.props.profile.showProfile}/>
-          </Grid>
+        <div className="App">
+          <Navbar />
+          <div className="container-fluid">
+            <Grid>
+              <WelcomePanel />
+              <SearchContainer />
+                <Row>
+                  <Button bsStyle="primary" bsSize="large" block
+                    onClick={this.getProfile}>
+                    Get My Track
+                  </Button>
+                </Row>
+              <ProfileContainer profile={this.props.profile.showProfile} />
+            </Grid>
+          </div>
         </div>
-      </div>
     );
   }
 }
