8ff4ad164a917075a4afc46ef2fe9c0c7830f850
TopTalentDev <toptal715@gmail.com>
2018-12-09 20:45:28 -0500

    minor update

--- client/src/presentation/Like.js
+++ client/src/presentation/Like.js
@@ -3,14 +3,14 @@ import { Button, Glyphicon, ButtonGroup } from 'react-bootstrap'
 
 function Like(props) {
   return (
-    <ButtonGroup>
+    <div className="button-container">
       <Button onClick={props.saveLike} bsSize="large">
         <Glyphicon glyph="thumbs-up" /> Save to my Profile
       </Button>
       <Button bsSize="large">
         <Glyphicon glyph="thumbs-down" /> Don't show me again
       </Button>
-    </ButtonGroup>
+    </div>
   )
 }
 
