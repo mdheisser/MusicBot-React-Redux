0fe4e00faf417fdbafe8733a0280aeb8431973f8
TopTalentDev <toptal715@gmail.com>
2018-12-09 20:43:15 -0500

    added log in form and profile presentation component

--- client/src/presentation/ProfileForm.js
+++ client/src/presentation/ProfileForm.js
@@ -2,19 +2,26 @@ import React, { Component } from 'react';
 import { FormGroup, FormControl, Form, ControlLabel, Button } from 'react-bootstrap'
 
 export default class ProfileForm extends Component {
+  spotifyAuth = () => {
+    
+  }
+
   render() {
     return (
-      <Form inline>
-        <FormGroup controlId="formInlineName">
-          <ControlLabel>Name</ControlLabel>{' '}
-          <FormControl type="text" placeholder="Jane Doe" />
-        </FormGroup>{' '}
-        <FormGroup controlId="formInlineEmail">
-          <ControlLabel>Email</ControlLabel>{' '}
-          <FormControl type="email" placeholder="jane.doe@example.com" />
-        </FormGroup>{' '}
-        <Button type="submit">Create Profile</Button>
-      </Form>
+      <>
+        <button onClick={this.spotifyAuth}>Connect with Spotify</button>
+        <Form inline>
+          <FormGroup controlId="formInlineName">
+            <ControlLabel>Name</ControlLabel>{' '}
+            <FormControl type="text" placeholder="Jane Doe" />
+          </FormGroup>{' '}
+          <FormGroup controlId="formInlineEmail">
+            <ControlLabel>Email</ControlLabel>{' '}
+            <FormControl type="email" placeholder="jane.doe@example.com" />
+          </FormGroup>{' '}
+          <Button type="submit">Create Profile</Button>
+        </Form>
+      </>
     )
   }
 }
---
+++ client/src/presentation/ProfilePage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ProfilePage = () => (
+  <h2>My Likes: </h2>
+)
+
+export default ProfilePage;
