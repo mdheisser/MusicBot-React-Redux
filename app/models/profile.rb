4f61bf2bf424d38cc26e1d4dbb5282f85ac6b782
TopTalentDev <toptal715@gmail.com>
2018-12-09 20:44:10 -0500

    updated like and recommend logic

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -9,43 +9,51 @@ class Profile < ApplicationRecord
       artist_ids.each do |id|
         artist = Artist.find_or_create_by(spotify_id: id)
         Like.create(profile_id: self.id, artist_id: artist.id, category: 'artist', spotify_id: artist.spotify_id)
-        self.artists << artist
       end
     end
     if track_ids.length > 0
       track_ids.each do |id|
         track = Track.find_or_create_by(spotify_id: id)
         Like.create(profile_id: self.id, track_id: track.id, category: 'track', spotify_id: track.spotify_id)
-        self.tracks << track
       end
     end
     self.save
   end
 
   def get_rec(token)
-    if self.tracks.length > 0
-      track = self.tracks.last.spotify_id
-    else
-      track = ''
+    track_ids = ''
+    artist_ids = ''
+    tracks = self.likes.select {|like| like.category === 'track'}
+    artists = self.likes.select {|like| like.category === 'artist'}
+    if self.likes.count > 5
+      tracks = tracks.last(2)
+      artists = artists.last(2)
     end
-    if self.artists.length > 0
-      artist = self.artists.last.spotify_id
-    else
-      artist = ''
+    if tracks.length > 0
+      track_ids = tracks.map {|track| track.spotify_id}.join('%20')
     end
+    if artists.length > 0
+      artist_ids = artists.map {|artist| artist.spotify_id}.join('%20')
+    end
+    resp = fetch_recommend(token, track_ids, artist_ids)
+    tracks = JSON.parse(resp.body)['tracks']
+    return tracks
+  end
+
+  private
+
+  def fetch_recommend(token, track_ids, artist_ids)
     resp = Faraday.get("https://api.spotify.com/v1/recommendations") do |req|
       req.headers = {
           'Content-Type': 'application/x-www-form-urlencoded',
           'Authorization': token
       }
       req.params = {
-        'limit': 1,
-        'seed_artists': artist,
-        'seed_tracks': track
+        'limit': 10,
+        'seed_artists': artist_ids,
+        'seed_tracks': track_ids
       }
     end
-    track = JSON.parse(resp.body)['tracks'].first
-    return track
   end
 
 end
