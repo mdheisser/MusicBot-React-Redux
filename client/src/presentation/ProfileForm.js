033992d4049b081f5ef4f14bf16ea685d37503ed
TopTalentDev <toptal715@gmail.com>
2018-12-11 08:45:34 -0500

    updated profile sign in form and logic

--- client/src/presentation/ProfileForm.js
+++ client/src/presentation/ProfileForm.js
@@ -2,24 +2,51 @@ import React, { Component } from 'react';
 import { FormGroup, FormControl, Form, ControlLabel, Button } from 'react-bootstrap'
 
 export default class ProfileForm extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+      email: ''
+    }
+  }
+
   spotifyAuth = () => {
-    
+
+  }
+
+  handleSubmit = () => {
+    const name = this.state.name
+    const email = this.state.email
+    const profileID = this.props.profileID
+    this.props.submit(name, email)
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
   }
 
   render() {
     return (
       <>
-        <button onClick={this.spotifyAuth}>Connect with Spotify</button>
+        <button onClick={this.spotifyAuth}>
+          Connect with Spotify</button>
         <Form inline>
           <FormGroup controlId="formInlineName">
             <ControlLabel>Name</ControlLabel>{' '}
-            <FormControl type="text" placeholder="Jane Doe" />
+            <FormControl name="name"
+              type="text" placeholder="Jane Doe"
+              value={this.state.name} onChange={this.handleChange}/>
           </FormGroup>{' '}
           <FormGroup controlId="formInlineEmail">
             <ControlLabel>Email</ControlLabel>{' '}
-            <FormControl type="email" placeholder="jane.doe@example.com" />
+            <FormControl name="email"
+              type="email" placeholder="jane.doe@example.com"
+              value={this.state.email} onChange={this.handleChange} />
           </FormGroup>{' '}
-          <Button type="submit">Create Profile</Button>
+          <Button type="submit" onClick={this.submitForm}>
+            Create Profile</Button>
         </Form>
       </>
     )
