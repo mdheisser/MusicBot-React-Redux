64bd7b311d6a3ce2009e5e44d86f68c0990fead6
TopTalentDev <toptal715@gmail.com>
2019-01-08 11:07:45 -0500

    added profile sign in route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/api/spotify/token', to: 'spotify#token'
   get '/api/spotify/search', to: 'spotify#search'
+  post '/api/spotify/save', to: 'spotify#save_search'
   post '/api/profiles', to: 'profile#create'
   post '/api/profiles/:profile_id/recommendations', to: 'profile#recommend'
   post '/api/profiles/:profile_id/likes', to: 'profile#like'
