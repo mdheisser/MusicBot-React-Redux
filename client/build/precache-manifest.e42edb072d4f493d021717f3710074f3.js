39f1838e0e0c605ad86499bda5db737173fbf26e
TopTalentDev <toptal715@gmail.com>
2019-01-08 22:07:16 -0500

    Adding `build` to source control

---
+++ client/build/precache-manifest.e42edb072d4f493d021717f3710074f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "47307ed31b1a99cc3e81",
+    "url": "/static/css/main.5835e097.chunk.css"
+  },
+  {
+    "revision": "47307ed31b1a99cc3e81",
+    "url": "/static/js/main.47307ed3.chunk.js"
+  },
+  {
+    "revision": "c90740ad9bffd8561b74",
+    "url": "/static/js/1.c90740ad.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "dc523540a3cde504d3dff41f525d4e53",
+    "url": "/static/media/AE7i.dc523540.gif"
+  },
+  {
+    "revision": "3a347e26453d83837009eb1b27e19315",
+    "url": "/index.html"
+  }
+];
